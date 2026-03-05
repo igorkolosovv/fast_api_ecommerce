14bff90e9afb383850408aea218553068f390978
Igor Kolosov <ikolosov06@gmail.com>
2026-03-05 15:51:05 +0300

    5.6.4

--- app/routers/category.py
+++ app/routers/category.py
@@ -14,7 +14,7 @@ router = APIRouter(prefix='/category', tags=['category'])
 
 @router.get('/all_categories')
 async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
-    categories = db.scalars(select(Category).where(Category.is_active == True)).all()
+    categories = db.scalars(select(Category)).all()
     return categories
 
 
@@ -31,8 +31,24 @@ async def create_category(db: Annotated[Session, Depends(get_db)], create_catego
 
 
 @router.put('/update_category')
-async def update_category():
-    pass
+async def update_category(db: Annotated[Session, Depends(get_db)], category_id: int, update_category: CreateCategory):
+    category = db.scalar(select(Category).where(Category.id == category_id))
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no category found'
+        )
+
+    db.execute(update(Category).where(Category.id == category_id).values(
+            name=update_category.name,
+            slug=slugify(update_category.name),
+            parent_id=update_category.parent_id))
+
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Category update is successful'
+    }
 
 
 @router.delete('/delete')

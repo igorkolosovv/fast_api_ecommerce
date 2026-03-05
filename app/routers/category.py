cc313195290f33b5afe54017bccd5e70be3edd86
Igor Kolosov <ikolosov06@gmail.com>
2026-03-05 15:32:52 +0300

    5.6.3 - добавление ручек

--- app/routers/category.py
+++ app/routers/category.py
@@ -4,7 +4,7 @@ from app.backend.db_depends import get_db
 from typing import Annotated
 
 from app.models import *
-from sqlalchemy import insert
+from sqlalchemy import insert, select, update
 from app.schemas import CreateCategory
 
 from slugify import slugify
@@ -13,8 +13,9 @@ router = APIRouter(prefix='/category', tags=['category'])
 
 
 @router.get('/all_categories')
-async def get_all_categories():
-    pass
+async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
+    categories = db.scalars(select(Category).where(Category.is_active == True)).all()
+    return categories
 
 
 @router.post('/create')
@@ -35,5 +36,16 @@ async def update_category():
 
 
 @router.delete('/delete')
-async def delete_category():
-    pass
+async def delete_category(db: Annotated[Session, Depends(get_db)], category_id: int):
+    category = db.scalar(select(Category).where(Category.id == category_id))
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no category found'
+        )
+    db.execute(update(Category).where(Category.id == category_id).values(is_active=False))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Category delete is successful'
+    }

b346fd98042b8540006220a5a91be1df5e579017
Igor Kolosov <ikolosov06@gmail.com>
2026-03-10 13:53:21 +0300

    5.12.3

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -40,3 +40,35 @@ async def supplier_permission(db: Annotated[AsyncSession, Depends(get_db)], get_
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You don't have admin permission"
         )
+
+
+@router.delete('/delete')
+async def delete_user(db: Annotated[AsyncSession, Depends(get_db)], get_user: Annotated[dict, Depends(get_current_user)], user_id: int):
+    if get_user.get('is_admin'):
+        user = await db.scalar(select(User).where(User.id == user_id))
+
+        if user.is_admin:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You can't delete admin user"
+            )
+
+        if user.is_active:
+            await db.execute(update(User).where(User.id == user_id).values(is_active=False))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is deleted'
+            }
+        else:
+            await db.execute(update(User).where(User.id == user_id).values(is_active=True))
+            await db.commit()
+            return {
+                'status_code': status.HTTP_200_OK,
+                'detail': 'User is activated'
+            }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You don't have admin permission"
+        )
